8ea23006825085d7aded563bc93624fdb190702f
???? ??? <you@vevo.uz>
2022-01-28 10:05:06 +0500

    build

---
+++ build/precache-manifest.b69941ea21f1eeee985acc1e9807bfb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "76b8d084b14dd75b2182",
+    "url": "/static/css/main.2834d344.chunk.css"
+  },
+  {
+    "revision": "76b8d084b14dd75b2182",
+    "url": "/static/js/main.76b8d084.chunk.js"
+  },
+  {
+    "revision": "a4b75cbcdb8b360e3fdd",
+    "url": "/static/css/1.141c2e4f.chunk.css"
+  },
+  {
+    "revision": "a4b75cbcdb8b360e3fdd",
+    "url": "/static/js/1.a4b75cbc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5c11e8dba5b3d2dfee139094345c46c1",
+    "url": "/static/media/rekit-react.5c11e8db.png"
+  },
+  {
+    "revision": "2c15504b8d1f9c0a4275f6e111aa5c07",
+    "url": "/index.html"
+  }
+];
